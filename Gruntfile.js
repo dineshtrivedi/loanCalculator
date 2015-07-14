186c045662997ad1b04efd1da2f0433459dac7b1
Dinesh <dinesh.dart@gmail.com>
2015-07-13 22:48:04 -0300

    Using bower_concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,22 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-      jshint: {
-        all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
-      }
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+        },
+        bower_concat: {
+            all: {
+                dest: 'build/_bower.js',
+                cssDest: 'build/_bower.css',
+                bowerOptions: {
+                    relative: false
+                }
+            }
+        }      
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bower-concat');
 
     grunt.registerTask('default', ['jshint']);
 
