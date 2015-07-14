f11dac91d8a3c6def0c937fde222efa5799d085c
Dinesh <dinesh.dart@gmail.com>
2015-07-14 00:35:08 -0300

    Ajustes da tela + TODOs

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # loanCalculator
+
+## TODO
+- Mudar nome do repositorio
+- Traduzir para portugues
+- Adicionar campo de valor total
+- Adicionar campo de porcentagem da entrada
+- Adicionar campo de valor financiado
+- Transformar todos inputs em uma diretiva
+- Criar diretiva pagamento-simples: Diretiva que representa valor unico para parcela
+- Criar calculo em cima do valor devido simulando a caixa economica federal
+- Criar diretiva para calculo da simulacao da caixa economica federal
+- Adicionar link para TR
+- Adicionar link para simulador da caixa
+- Exibir valor perdido com juros (financiamento pela caixa)
+- Criar diretiva que permita colocar valor de aluguel para calcular amortizacao do juros
+- Criar diretiva que permita adicionar pagamentos pontuais (mes x valor) para exemplo de uso do fgts
--- src/app.js
+++ src/app.js
@@ -13,19 +13,27 @@ angular.module('loanCalculator').factory('LoanModel', function(){
     var m = {
         calculateCurrentPayment: calculateCurrentPayment,
         init: init,
+        calculateSimplePayment: calculateSimplePayment,
     };
 
     function calculateCurrentPayment(amount, rate, numberPayment){
-        var top = (amount * rate)*Math.pow((1+rate), numberPayment);
+        var top = (amount * rate)*(Math.pow((1+rate), numberPayment));
         var base = Math.pow((1+rate), numberPayment) - 1;
         return top/base;
     }
 
+    function calculateSimplePayment(){
+        var rateByMonth = (m.rate/100 + m.TR)/12;
+        var value = m.calculateCurrentPayment(m.totalAmount, rateByMonth, m.numberPayment);
+        m.simplePayment = Math.round(value * 100) / 100
+    }
+
     function init(){
-        m.totalAmount = 0;
+        m.totalAmount = 195500;
         m.rate = 7.8;
-        m.TR = 0.3;
+        m.TR = 0.03;
         m.numberPayment = 360;
+        m.simplePayment = 0;
     }
     return m;
 })
