0d779a84e8e6469a01db5c028c4e2220953c1bee
Dinesh <dinesh.dart@gmail.com>
2015-07-13 23:30:45 -0300

    initial inputs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 angular.module('loanCalculator', []);
+
+angular.module('loanCalculator').factory('LoanModel', function(){
+    var m = {
+        calculateCurrentPayment: calculateCurrentPayment,
+        init: init,
+    };
+
+    function calculateCurrentPayment(amount, rate, numberPayment){
+        var top = (amount * rate)*Math.pow((1+rate), numberPayment);
+        var base = Math.pow((1+rate), numberPayment) - 1;
+        return top/base;
+    }
+
+    function init(){
+        m.totalAmount = 0;
+        m.rate = 7.8;
+        m.TR = 0.3;
+        m.numberPayment = 360;
+    }
+    return m;
+})
+angular.module('loanCalculator').controller('LoanCtrl', function($scope, LoanModel){
+    var m = $scope.m = LoanModel;
+    m.init();
+});
